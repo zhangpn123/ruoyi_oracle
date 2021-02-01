7d9f86a60774d43ea954e3d9cd988e14c84d723e
zhangpuning <zhangpuning123@163.com>
2021-02-01 14:11:15 +0800

    自定义查询添加条件

--- src/main/java/com/ruoyi/project/report/customquert/controller/CustomqueryController.java
+++ src/main/java/com/ruoyi/project/report/customquert/controller/CustomqueryController.java
@@ -9,7 +9,9 @@ import com.ruoyi.project.report.customquert.dto.CustomPageReq;
 import com.ruoyi.project.report.customquert.service.CustomqueryService;
 import com.ruoyi.project.system.dict.domain.DictData;
 import com.ruoyi.project.system.dict.service.IDictDataService;
+import com.ruoyi.project.system.user.domain.User;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -56,25 +58,41 @@ public class CustomqueryController extends BaseController {
     public TableDataInfo getData(CustomPageReq customPageReq) {
         log.info("开始自定义分页查询");
         DictData dictData = dictDataService.selectDictDataById(Long.parseLong(customPageReq.getDictCode()));
+        String customSql = dictData.getDictValue();
+        StringBuffer sb = new StringBuffer();
+        int index = customSql.indexOf("1=1");
+        if (index != -1){
+            /*将sql分割*/
+            String prefix = customSql.substring(0, index + 3);
+            String suffix = customSql.substring(index + 3,customSql.length());
+            if(StringUtils.isEmpty(customPageReq.getDeptId())){
+                User user = (User) SecurityUtils.getSubject().getPrincipal();
+                /*拼接条件*/
+                customSql = sb.append(prefix).append(" AND co_code like '"+user.getDeptId()+"%' ").append(suffix).toString();
+            }else{
+                customSql = sb.append(prefix).append(" AND co_code = '"+customPageReq.getDeptId()+"' ").append(suffix).toString();
+            }
+        }
+
         startPage();
-        List<Map<String, Object>> customResult = customqueryService.selectList(dictData.getDictValue());
+        List<Map<String, Object>> customResult = customqueryService.selectList(customSql);
         return getDataTable(customResult);
     }
 
 
     @PostMapping("/getHeaderList")
     @ResponseBody
-    public TableDataInfo getHeaderList(@RequestParam("dictCode") String dictCode) {
+    public TableDataInfo getHeaderList(CustomPageReq customPageReq) {
         log.info("开始获取查询的表头");
         /*校验数据*/
         TableDataInfo rspData = new TableDataInfo();
-        if (StringUtils.isEmpty(dictCode)) {
-            log.error("要查询的数据为空");
+        if (StringUtils.isEmpty(customPageReq.getDictCode())) {
+            log.error("要查询的sql条件为空");
             rspData.setCode(301);
             rspData.setMsg("请选择要查询的条件");
             return rspData;
         }
-        DictData dictData = dictDataService.selectDictDataById(Long.parseLong(dictCode));
+        DictData dictData = dictDataService.selectDictDataById(Long.parseLong(customPageReq.getDictCode()));
         if(dictData == null || StringUtils.isEmpty(dictData.getDictValue())){
             log.error("要查询的数据为空");
             rspData.setCode(301);
@@ -90,16 +108,37 @@ public class CustomqueryController extends BaseController {
             return rspData;
         }
         try {
-            PageHelper.startPage(1, 1);
+            StringBuffer sb = new StringBuffer();
+            int index = sql.indexOf("1=1");
+            if (index != -1){
+                /*将sql分割*/
+                String prefix = sql.substring(0, index + 3);
+                String suffix = sql.substring(index + 3,sql.length());
+                if(StringUtils.isEmpty(customPageReq.getDeptId())){
+                    User user = (User) SecurityUtils.getSubject().getPrincipal();
+                    /*拼接条件*/
+                    sql = sb.append(prefix).append(" AND co_code like '"+user.getDeptId()+"%' ").append(suffix).toString();
+                }else{
+                    sql = sb.append(prefix).append(" AND co_code = '"+customPageReq.getDeptId()+"' ").append(suffix).toString();
+                }
+            }
+            PageHelper.startPage(1, 1,false);
             List<Map<String, Object>> customResult = customqueryService.selectList(sql);
-            LinkedHashMap custom = (LinkedHashMap)customResult.get(0);
-            custom.remove("ROW_ID");
-            Map customMap = new LinkedHashMap();
-            customMap.put("ROW_ID","ROW_ID");
-            customMap.putAll(custom);
-            customResult.clear();
-            customResult.add(customMap);
-            return getDataTable(customResult);
+            if(customResult != null && customResult.size() > 0){
+                LinkedHashMap custom = (LinkedHashMap)customResult.get(0);
+                custom.remove("ROW_ID");
+                Map customMap = new LinkedHashMap();
+                customMap.put("ROW_ID","ROW_ID");
+                customMap.putAll(custom);
+                customResult.clear();
+                customResult.add(customMap);
+                return getDataTable(customResult);
+            }else{
+                rspData.setCode(201);
+                rspData.setMsg("查询的数据为空");
+                return rspData;
+            }
+
         } catch (Exception e) {
             log.error("自定义查询失败!", e);
             rspData.setCode(500);
--- src/main/java/com/ruoyi/project/report/customquert/dto/CustomPageReq.java
+++ src/main/java/com/ruoyi/project/report/customquert/dto/CustomPageReq.java
@@ -11,4 +11,5 @@ import lombok.Data;
 @Data
 public class CustomPageReq {
     private String dictCode;
+    private String deptId;
 }
